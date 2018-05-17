af3fb98e22a90d74cd9d5e5bbf15483949f30da8
Eliasr123 <te16.elias.renman@umea.nti.se>
2018-05-17 16:43:11 +0200

    Loads a BufferedImage with a certain name"filename"

---
+++ src/main/java/discordBot/main/FileUtil/FileManager.java
@@ -0,0 +1,18 @@
+package discordBot.main.FileUtil;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class FileManager {
+    public BufferedImage load(String filename) {
+        try {
+            System.out.println("Loading succeeded");
+           return ImageIO.read(new File("ImagesDownloaded/"+ filename));
+        } catch (IOException e) {
+            System.out.println("Image Load Failed!");
+        }
+        return null;
+    }
+}
