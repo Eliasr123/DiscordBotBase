60578730591f60066aa0879aa537c1404ec16a0f
eliasr123 <te16.elias.renman@umea.nti.se>
2018-05-18 16:08:27 +0200

    updated javadoc text to be accurate

--- src/main/java/discordBot/main/FileUtil/Attachments.java
+++ src/main/java/discordBot/main/FileUtil/Attachments.java
@@ -10,7 +10,7 @@ public class Attachments {
     /** function tris to download a attached file from discord
      * @param message inputted message that might contain a attachment
      * @param filterImages decides if it only wants to save images or if any file is fine
-     * @return returns a boolean depending if the saving was possible or not */
+     * @return returns the file path that the image was saved to*/
     public static File download(Message message, boolean filterImages,String directory) {
         for(Message.Attachment attachment : message.getAttachments()) {
             if(!filterImages || attachment.isImage()) {
