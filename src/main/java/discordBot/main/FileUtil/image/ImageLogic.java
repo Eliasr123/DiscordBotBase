a75183e63cbd9ec002ea517ad49b82d616dcbdc2
eliasr123 <te16.elias.renman@umea.nti.se>
2018-05-18 16:22:52 +0200

    initial commit, non-functioning as of now

---
+++ src/main/java/discordBot/main/FileUtil/image/ImageLogic.java
@@ -0,0 +1,37 @@
+package discordBot.main.FileUtil.image;
+
+import discordBot.main.FileUtil.FileManager;
+import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.MessageChannel;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.Arrays;
+
+public class ImageLogic {
+    private static FileManager fileManager = new FileManager();
+
+    /**
+     *
+     * @param objChannel
+     */
+    public void compareImage(MessageChannel objChannel,Message message, File filePath) {
+        BufferedImage ref = fileManager.load(filePath);
+        //BufferedImage saber = fileManager.load();
+        BufferedImage[] refs = loadCompareRefs();
+
+
+        objChannel.sendMessage("Match position " + Arrays.toString(Compare.findSubImage(ref,saber))).queue();
+        objChannel.sendMessage("Match " + Compare.findSubImageD(ref,saber) + "%").queue();
+
+        objChannel.sendMessage("Match position " + Arrays.toString(Compare.findSubImage(ref,saber))).queue();
+        objChannel.sendMessage("Match " + Compare.findSubImageD(ref,saber) + "%").queue();
+    }
+    private BufferedImage[] loadCompareRefs() {
+        BufferedImage[] imageArray = new BufferedImage[8];
+        for (int i = 0; i < imageArray.length;i++) {
+            imageArray[i] = fileManager.load(new File("ImagesDownloaded/Refs/item"+i+".png"));
+        }
+        return imageArray;
+    }
+}
