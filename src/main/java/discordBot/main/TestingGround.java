64b682d482b986cd494434f42696ce75011b05cd
Abborren <33152039+Abborren@users.noreply.github.com>
2018-05-20 22:33:54 +0200

    added the needed testing tools for testing the finding of subImages

--- src/main/java/discordBot/main/TestingGround.java
+++ src/main/java/discordBot/main/TestingGround.java
@@ -2,12 +2,19 @@ package discordBot.main;
 
 import discordBot.main.FileUtil.image.ImageLogic;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
 
 public class TestingGround {
     private static ImageLogic imageLogic = new ImageLogic();
     public static void main(String[] args) {
-        imageLogic.compareImageTest(new File("staticrefser.png"),new File("ref4.png"));
+        BufferedImage ref = null;
+        ref = ImageLogic.fileManager.load(new File("ImagesDownloaded/StaticRef1.png"));
+        if (ref != null) {
+            imageLogic.createSubImages(ref);
+        }
+
+        //imageLogic.compareImageTest(new File("ImagesDownloaded/staticrefser.png"),new File("ImagesDownloaded/ref4.png"));
 
     }
     private void runTest() {
