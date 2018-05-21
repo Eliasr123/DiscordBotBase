23e565d4b9b986b8d5b71b0097e9566ef6d2a1ff
eliasr123 <te16.elias.renman@umea.nti.se>
2018-05-21 07:20:00 +0200

    moved directories the Image Directories

--- src/main/java/discordBot/main/TestingGround.java
+++ src/main/java/discordBot/main/TestingGround.java
@@ -9,7 +9,7 @@ public class TestingGround {
     private static ImageLogic imageLogic = new ImageLogic();
     public static void main(String[] args) {
         BufferedImage ref = null;
-        ref = ImageLogic.fileManager.load(new File("ImagesDownloaded/StaticRef1.png"));
+        ref = ImageLogic.fileManager.load(new File("Images/Downloaded/StaticRef1.png"));
         if (ref != null) {
             imageLogic.createSubImages(ref);
         }
