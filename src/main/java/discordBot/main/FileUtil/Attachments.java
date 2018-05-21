58f1d2f7c41ea73d17110182c44296d236781837
eliasr123 <te16.elias.renman@umea.nti.se>
2018-05-21 08:26:46 +0200

    made two functions one that retains the name of the attachment and another that gives it a new name

--- src/main/java/discordBot/main/FileUtil/Attachments.java
+++ src/main/java/discordBot/main/FileUtil/Attachments.java
@@ -7,14 +7,23 @@ import java.io.File;
 public class Attachments {
 
 
-    /** function tris to download a attached file from discord
+    /** function tris to downloadRetainName a attached file from discord
      * @param message inputted message that might contain a attachment
      * @param filterImages decides if it only wants to save images or if any file is fine
      * @return returns the file path that the image was saved to*/
-    public File download(Message message, boolean filterImages,String directory) {
+    public File downloadRetainName(Message message, boolean filterImages, String directory) {
         for(Message.Attachment attachment : message.getAttachments()) {
             if(!filterImages || attachment.isImage()) {
                 File file = new File(directory + attachment.getFileName());
+                attachment.download(file);
+                return file;
+            }
+        }
+        return null;
+    }
+    public File downloadChangeName(Message message, boolean filterImages, File file) {
+        for(Message.Attachment attachment : message.getAttachments()) {
+            if(!filterImages || attachment.isImage()) {
                 attachment.download(file);
                 return file;
             }
