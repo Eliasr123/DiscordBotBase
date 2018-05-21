398f4655264fd7c46920596c423dddf3c3a3c404
eliasr123 <te16.elias.renman@umea.nti.se>
2018-05-21 07:37:46 +0200

    moved package..

--- src/main/java/discordBot/main/TestingGround.java
+++ src/main/java/discordBot/main/TestingGround.java
@@ -1,5 +1,6 @@
 package discordBot.main;
 
+import discordBot.main.FileUtil.image.CropImage;
 import discordBot.main.FileUtil.image.ImageLogic;
 
 import java.awt.image.BufferedImage;
@@ -7,12 +8,14 @@ import java.io.File;
 
 public class TestingGround {
     private static ImageLogic imageLogic = new ImageLogic();
+    private static CropImage cropImage = new CropImage();
     public static void main(String[] args) {
         BufferedImage ref = null;
         ref = ImageLogic.fileManager.load(new File("Images/Downloaded/StaticRef1.png"));
         if (ref != null) {
-            imageLogic.createSubImages(ref);
+            cropImage.createSubImages(ref);
         }
+
 
         //imageLogic.compareImageTest(new File("ImagesDownloaded/staticrefser.png"),new File("ImagesDownloaded/ref4.png"));
 
