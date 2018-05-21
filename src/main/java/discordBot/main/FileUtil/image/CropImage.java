2edfab56b45ee826caafc7d0a44ef1c4774c182a
eliasr123 <te16.elias.renman@umea.nti.se>
2018-05-21 07:44:08 +0200

    removed old useless code and replaced it with new logic for finding reference points and cropping it.

--- src/main/java/discordBot/main/FileUtil/image/CropImage.java
+++ src/main/java/discordBot/main/FileUtil/image/CropImage.java
@@ -9,18 +9,45 @@ import java.io.IOException;
  * Put in picture, coordinates and size and it will spit back a image
  */
 public class CropImage {
-    private BufferedImage crop(int x,int y,int w,int h,String path, int i){
-        BufferedImage image = null;
-        try {
-            image = ImageIO.read(new File("ImagesDownloaded/"+path));
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.out.print("Image Load failed");
-        }
-        return image.getSubimage(x, y, w, h);
+    private Compare compare = new Compare();
+    public void createSubImages(BufferedImage bigImage) {
+        // use the Border.png to find the first border.
+        BufferedImage border = ImageLogic.fileManager.load(new File("Images/Resources/Border/Border.png"));
+        int[] coordinates = compare.findSubImage(bigImage, border);
+        //System.out.println(coordinates[0] + " - " + coordinates[1]);
 
-    }
-    public int[] findRelativePos() {
-        return null;
+        // use IMAGE_CONSTANTS.IMAGE_WIDTH and IMAGE_CONSTANTS.IMAGE_HEIGHT to cut out
+        // the image.
+
+
+        // do this for all images.
+        // after the first image, there are 4 rows down at 100 pixels lower than the previous.
+        // after the first image, there are 6 colums to the right, at 58 pixels difference
+        // after the 6th image, there is a gap of 90 before the above statement is repeated.
+        int counter = 0;
+        int refNumber = 0;
+        for(int rows = 0; rows < 3; rows++) {
+            for(int columns = 0; columns < 12; columns++) {
+                counter++;
+                BufferedImage subImage = bigImage.getSubimage(columns<6?coordinates[0]+(columns*58):coordinates[0]+32+(columns*58), coordinates[1]+(rows*100) ,48, 48);
+
+                if (counter == 5) {
+                    try {
+                        ImageIO.write(subImage, "png", new File(String.format("Images/Resources/Output/ref%s.png", refNumber)));
+                        refNumber++;
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    counter = -1;
+                }
+                if (rows== 2 && columns ==7) {
+                    break;
+                }
+            }
+        }
+
+
+
+        // write the subimages to disk, this should create 42 images.
     }
 }
