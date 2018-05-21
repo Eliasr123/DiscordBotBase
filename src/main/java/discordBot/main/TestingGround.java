e7d22bf62685f1372c87598459c01adbbb2036e2
eliasr123 <te16.elias.renman@umea.nti.se>
2018-05-21 14:50:34 +0200

    testingGround. still not working Q_Q

--- src/main/java/discordBot/main/TestingGround.java
+++ src/main/java/discordBot/main/TestingGround.java
@@ -1,5 +1,7 @@
 package discordBot.main;
 
+import discordBot.main.FileUtil.FileManager;
+import discordBot.main.FileUtil.image.Compare;
 import discordBot.main.FileUtil.image.CropImage;
 import discordBot.main.FileUtil.image.ImageLogic;
 
@@ -9,8 +11,12 @@ import java.io.File;
 public class TestingGround {
     private static CropImage cropImage = new CropImage();
     private static ImageLogic imageLogic = new ImageLogic();
+    private static Compare compare = new Compare();
+    private static FileManager fileManager = new FileManager();
     public static void main(String[] args) {
-        BufferedImage inputImg = null;
-        imageLogic.compareImageTest();
+        BufferedImage inputImg = fileManager.load(new File("Images/Downloaded/Refs/ref2.png"));
+        BufferedImage tradeImg = fileManager.load(new File("Images/Downloaded/Input/ref2.png"));
+        //imageLogic.compareImageTest();
+        compare.findSubImage(inputImg,tradeImg,0.01);
     }
 }
